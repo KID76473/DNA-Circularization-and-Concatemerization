e3cd6b5a5e488ef80d9a5d47e26b7ba071cd2774
James Huang <kid76473@gmail.com>
2024-06-05 11:32:36 -0700

    'graph'

--- npyReader.py
+++ npyReader.py
@@ -1,14 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 heads = np.load('heads.npy')
-print(heads[0, 0, 0])
-
 furthest = np.load('furthest.npy')
-print(furthest[0, 0, 0])
-
 cir = np.load('circularization.npy')
-print(f"circularization: {cir}")
-
 con = np.load('concatemerization.npy')
+
+num = 10
+print(f"circularization: {cir}")
 print(f"concatemerization: {con}")
+
+label = []
+for i in range(num):
+    label.append([cir[i], con[i]])
+
+fig, ax = plt.subplots()
+
+plt.plot(range(24, 24 + num), [1] * num, color='red')
+plt.scatter(range(24, 24 + num), cir / con)
+plt.xlabel("Distance between DNA Molecule(10 simulations for every distance)")
+plt.ylabel("Ratio of Circularization / Concatemerization")
+ax.set_title("Ratio of Circularization / Concatemerization \nover 10 Simulations for each distance from 24 to 34")
+
+for i, l in enumerate(label):
+    ax.text(24 + i, cir[i] / con[i], l)
+
+plt.grid(True)
+plt.show()
