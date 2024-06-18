96351a5118a30e5dfb55d3c78bac2c344b8f02f3
jin <jzhou2@caltech.edu>
2024-06-18 13:56:48 -0700

    added a venv

--- monte_carlo_formula_dep_con.py
+++ monte_carlo_formula_dep_con.py
@@ -1,11 +1,14 @@
 import numpy as np
+import sys
 
 radius = 1 #closeness of DNA spheres where concatemerization happens
 conc_start = 1
-conc_end = 100
+conc_end = 200
+
+output_filename = open("monte_carlo_out", "a")
 
 for conc in range(conc_start, conc_end + 1):
     #monte carlo formula based on conc of DNA spheres
     num = 4 * np.pi * radius ** 3 
     denom = 3 * conc ** 3
-    print(f"conc: {conc}; concat_value: {num / denom}")
+    output_filename.write(f"conc: {conc}; concat_value: {num / denom} \n")
