52a69ea9903bca48cb9d5fed8dd2270cf961d530
jin <jzhou2@caltech.edu>
2024-06-20 13:28:27 -0700

    parsing ER and MC outputs, made log scale graphs

---
+++ parsing_eventual_return.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+x_vals =[]
+y_vals = []
+
+file = open("eventual_return_formula_output_1", "r")
+
+for line in file:
+    split = line.split()
+    x_val = split[1]
+    x_val = x_val[:len(x_val) - 1]
+    x_vals.append(float(x_val))
+    y_val = split[4]
+    y_vals.append(float(y_val))
+
+plt.plot(x_vals, y_vals)
+plt.yscale('log')
+plt.xlabel('length')
+plt.ylabel('calculated return probability (circularization)')
+plt.title('length vs calculated circularization prob')
+plt.grid(True)
+plt.show()
