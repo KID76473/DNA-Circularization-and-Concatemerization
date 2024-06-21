b7b4b8dbb053f22cae613dd72a44238b559458bc
lab computer <kid76473@gmail.com>
2024-06-21 16:56:31 -0700

    lab

--- controller.py
+++ controller.py
@@ -2,10 +2,16 @@ import concurrent.futures
 import subprocess
 
 # Function to run the circularization.py script and save output to a file
-def run_circularization(thread_id):
+def run_circularization(thread_id, temp):
     try:
-        output_filename = f'output_thread_{thread_id}.txt'
-        result = subprocess.run(['python', 'circularization.py', output_filename], capture_output=True, text=True)
+        if temp:  # concatemerization
+            # output_filename = f'concat_thread_{thread_id}.txt'
+            # result = subprocess.run(['python', 'concatemerization_length.py', output_filename], capture_output=True, text=True)
+            output_filename = f'concat_thread_{thread_id}_concentration.txt'
+            result = subprocess.run(['python', 'concatemerization_concentration.py', output_filename], capture_output=True, text=True)
+        else:  # circularization
+            output_filename = f'circle_thread_{thread_id}.txt'
+            result = subprocess.run(['python', 'circularization.py', output_filename], capture_output=True, text=True)
         if result.returncode != 0:
             return f"An error occurred in thread {thread_id}: {result.stderr}"
         return output_filename
@@ -15,9 +21,10 @@ def run_circularization(thread_id):
 # Main function to execute the script using multi-threading
 def main():
     num_threads = 32
+    temp = 1  # 0 for circularization 1 for concatemerization
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        futures = [executor.submit(run_circularization, i) for i in range(num_threads)]
+        futures = [executor.submit(run_circularization, i, temp) for i in range(num_threads)]
 
         for future in concurrent.futures.as_completed(futures):
             try:
