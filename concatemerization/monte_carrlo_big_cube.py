595415a0c0018a72392c207ea5c7dca6a463c0e7
lab computer <kid76473@gmail.com>
2024-07-03 09:54:29 -0700

    plot

--- concatemerization/monte_carrlo_big_cube.py
+++ concatemerization/monte_carrlo_big_cube.py
@@ -2,33 +2,38 @@ import numpy as np
 from numba import jit
 
 
-@jit
+# @jit
 def func():
     N = 64
     dimensions = 3
-    concentrations = np.arange(2, 21, 19)
-    positions = np.zeros((N, N, N, dimensions))
-    num = 100000000000
+    concentrations = np.arange(2, 21)
+    num = 10000000
     concat = np.zeros(19)
     error = 1
+
+    with open("../data/big_cube", 'w') as f:
+        f.write("Started running\n")
 
     i = 0
     while i < num:
         for c in range(len(concentrations)):
-            positions = np.random.uniform(concentrations[c] * 10, size=(N, N, N, dimensions))
-            concat[c] = np.sum((positions < error).all(axis=-1))
+            positions = np.random.uniform(0, concentrations[c] * 10, size=(N, N, N, dimensions))
+            concat[c] += np.sum((positions < error).all(axis=-1))
         n = 10000
         # write the result to file every 100000 loops
         if i % n == 0:
-            # write_file(i, concat)
-            print(f"{i} loops")
-            print(str(concat / i) + "\n")
+            write_file(i, concat, concentrations)
+            # print(f"{i} loops")
+            # print(str(concat / i) + "\n")
         i += 1
 
-func()
 
-def write_file(i, concat):
+def write_file(i, concat, concentrations):
     with open("../data/big_cube", 'a') as f:
         f.write(f"{i} loops\n")
         # f.write(str(positions) + "\n")
-        f.write(str(concat / i) + "\n")
+        for c in range(len(concentrations)):
+            f.write(str(concentrations[c]) + ": " + str(concat[c] / ((i + 1) * 64 ^ 3)) + "\n")
+
+
+func()
---
+++ plot_big_cube.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+
+
+# Function to extract data from the file
+def extract_data(filename):
+    data = {}
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        current_loop = None
+        for line in lines:
+            if line.strip().endswith('loops'):
+                current_loop = int(line.split()[0])
+                data[current_loop] = {}
+            elif current_loop is not None:
+                parts = line.split(':')
+                key = int(parts[0].strip())
+                value = float(parts[1].strip())
+                data[current_loop][key] = value
+    return data
+
+
+# Function to plot the data of the last loop
+def plot_last_loop(data):
+    last_loop = max(data.keys())
+    x = list(data[last_loop].keys())
+    y = list(data[last_loop].values())
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, y, marker='o')
+    plt.title(f'Probability of Concatemerization vs. Concentration')
+    plt.xlabel('Concentration')
+    plt.ylabel('Probability of Concatemerization')
+    plt.grid(True)
+    plt.show()
+
+
+filename = 'data/big_cube'
+data = extract_data(filename)
+plot_last_loop(data)
