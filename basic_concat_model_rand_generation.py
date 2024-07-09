a5ac9916ed16e0a4535749c6ce5b29c13c3194f2
jin <jzhou2@caltech.edu>
2024-07-09 16:24:07 -0700

    2,4 square

---
+++ basic_concat_model_rand_generation.py
@@ -0,0 +1,35 @@
+import math
+import random as rand
+
+sphere_radius = 1
+num_tails = 10 ** 8
+
+side_len_of_norm_cube = 1.365 * (10 ** 7)
+vol_of_cube = side_len_of_norm_cube ** 3
+val1 = vol_of_cube / (1.955 * (10 ** 13))
+side_len = val1 * (10 ** 8)
+
+def comp_sphere(list: list, radius: int):
+    val1 = list[0] ** 2
+    val2 = list[1] ** 2
+    val3 = list[2] ** 2
+    distance = math.sqrt(val1 + val2 + val3)
+    if distance <= radius:
+        return True
+    else:
+        return False
+
+def run(num_tails, side_len_halved, radius):
+    cir = 0
+    for i in range(num_tails):
+        tail = [rand.uniform(-side_len_halved, side_len_halved), rand.uniform(-side_len_halved, side_len_halved), rand.uniform(-side_len_halved, side_len_halved)]
+        if comp_sphere(tail, radius):
+            cir += 1
+        if i % 100000 == 0:
+            print(f"tail_num {i} cir {cir}")
+    return cir
+
+cir = run(num_tails, side_len / 2, sphere_radius)
+file = open("big_cube_concat_24_square", "w")
+
+file.write(f"tail: {num_tails} cir: {cir}")
