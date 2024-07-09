9d0ae92fa874fac57a7cb857076cf7a68ea004ac
jin <jzhou2@caltech.edu>
2024-07-09 16:26:53 -0700

    num_trails

--- basic_concat_model_rand_generation.py
+++ basic_concat_model_rand_generation.py
@@ -4,6 +4,8 @@ import random as rand
 sphere_radius = 1
 num_tails = 10 ** 8
 
+num_trails = 10000
+
 side_len_of_norm_cube = 1.365 * (10 ** 7)
 vol_of_cube = side_len_of_norm_cube ** 3
 val1 = vol_of_cube / (1.955 * (10 ** 13))
@@ -21,12 +23,13 @@ def comp_sphere(list: list, radius: int):
 
 def run(num_tails, side_len_halved, radius):
     cir = 0
-    for i in range(num_tails):
-        tail = [rand.uniform(-side_len_halved, side_len_halved), rand.uniform(-side_len_halved, side_len_halved), rand.uniform(-side_len_halved, side_len_halved)]
-        if comp_sphere(tail, radius):
-            cir += 1
-        if i % 100000 == 0:
-            print(f"tail_num {i} cir {cir}")
+    for i in range(num_trails):
+        for j in range(num_tails):
+            tail = [rand.uniform(-side_len_halved, side_len_halved), rand.uniform(-side_len_halved, side_len_halved), rand.uniform(-side_len_halved, side_len_halved)]
+            if comp_sphere(tail, radius):
+                cir += 1
+            if i % 100000 == 0:
+                print(f"trial_num {i} tail_num {j} cir {cir}")
     return cir
 
 cir = run(num_tails, side_len / 2, sphere_radius)
