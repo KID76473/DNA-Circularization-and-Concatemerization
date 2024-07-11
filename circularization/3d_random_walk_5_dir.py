c4944cbcb9af5b73e63f72620f0af4b2112bef2d
lab computer <kid76473@gmail.com>
2024-07-11 10:58:07 -0700

    add

--- circularization/3d_random_walk_5_dir.py
+++ circularization/3d_random_walk_5_dir.py
@@ -4,10 +4,11 @@
 
 import random as rand
 import time
+from numba import jit
 
-num_trials = 25000000
+num_trials = 50000000
 
-len_list = [500, 1000, 1500, 2000]
+len_list = [5000, 10000, 20000, 50000]
 
 dir_list = [-3, -2, -1, 1, 2, 3]
 
@@ -16,10 +17,12 @@ y_vals = []
 
 start_pos = [0, 0, 0] #[x, y, z]
 
-file_output = open("basic_3d_rand_walk_5_dir_output", "w")
+file_output = open("jit_3d_rand_walk_5_dir_50_to_50k", "w")
 
 t0 = time.time()
 
+
+@jit
 def random_walk(pos, dir):
     if dir == -3: #east
         pos[0] += 1
@@ -35,28 +38,32 @@ def random_walk(pos, dir):
         pos[2] -= 1 
     return pos
 
-for num in len_list:
-    total_cir = 0
-    for i in range(num_trials):
-        pos = [0, 0, 0]
-        not_allowed = 0
+@jit
+def run(len_listi, dir_listi, num_trialsi):
+    for num in len_listi:
+        total_cir = 0
+        for i in range(num_trialsi):
+            pos = [0, 0, 0]
+            not_allowed = 0
 
-        for j in range(0, num):
-            direction = rand.choice(dir_list)
-            while direction == not_allowed:
-                direction = rand.choice(dir_list)
-            not_allowed = -direction
-            pos = random_walk(pos, direction)
+            for j in range(0, num):
+                direction = rand.choice(dir_listi)
+                while direction == not_allowed:
+                    direction = rand.choice(dir_listi)
+                not_allowed = -direction
+                pos = random_walk(pos, direction)
 
-        if pos == start_pos:
-            total_cir += 1
+            if pos == start_pos:
+                total_cir += 1
 
-        if i % 100000 == 0:
-            print(num, i, total_cir)
-        
-    x_vals.append(num)
-    y_vals.append(total_cir)
-    file_output.write(f"length: {num} total_cir: {total_cir} total: {num_trials} \n")
+            if i % 100000 == 0:
+                print(num, i, total_cir)
+
+        x_vals.append(num)
+        y_vals.append(total_cir)
+
+
+run(len_list, dir_list, num_trials)
 
 t1 = time.time()
 
