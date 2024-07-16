77e31c1117dafb307cc6fcda7346453d8462d032
lab computer <kid76473@gmail.com>
2024-07-16 10:42:08 -0700

    noback

--- circularization/circularization.py
+++ circularization/circularization.py
@@ -12,11 +12,12 @@ heads = np.zeros((4, 3))
 error = 1
 directions = direction_functions.get_directions(num_dir)
 concentration = 30
-deg = np.pi / 2
+deg = np.pi / 5
+n = 10000
 
 output_filename = sys.argv[1]
 with open("data/" + str(output_filename), 'w') as f:
-    f.write("Starting new run circularization??????\n")
+    f.write("Starting new run circularization:\n")
 
 t0 = time.time()
 i = 0
@@ -24,13 +25,14 @@ while i < num:
     # every case
     head = np.zeros(3)
     last = np.array([0, 0, 0])
+    # for loop for every case
     for j in range(length):
         # # any random direction
         # head += directions[np.random.choice(num_dir ** 2)]
 
         # choose random direction based on the last step
-        temp, length = direction_functions.fibonacci_sphere(last, deg)
-        temp = temp[np.random.choice(length)]
+        temp, upper_bound = direction_functions.fibonacci_sphere(last, deg)
+        temp = temp[np.random.choice(upper_bound)]
         head += temp
         last = temp
         if j in [499, 999, 1499, 1999]:
@@ -40,15 +42,13 @@ while i < num:
     for j in range(len(heads)):
         if (np.abs(heads[j]) < error).all():
             cirs[j] += 1
-    i += 1
-    n = 10000
     # write the result to file every 100000 loops
     if i % n == 0:
-        print(i)
         with open("data/" + str(output_filename), 'a') as f:
-            f.write(f"{i} loops\n")
+            f.write(f"{i + 1} loops\n")
             for k in range(len(cirs)):
                 if cirs[k] == 0:
                     f.write(f"i: {(k + 1) * 500}, head: {heads[k]}, no cir\n")
                 else:
                     f.write(f"i: {(k + 1) * 500}, head: {heads[k]}, cir: {cirs[k]}, cir / num: {cirs[k] / i}\n")
+    i += 1
